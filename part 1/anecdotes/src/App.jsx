dafd51cd79b37763044ccf3a5e9d8242b449b021
Chirag <69969661+C-h-i-rag@users.noreply.github.com>
2023-11-01 10:09:51 +0530

    Add Ancedote Exercises

--- part 1/anecdotes/src/App.jsx
+++ part 1/anecdotes/src/App.jsx
@@ -1,35 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0,4:0,5:0,6:0,7:0 })
+
+  
+  const nextAnecdotes = () => setSelected(getRandomInt(0, anecdotes.length - 1));
+
+  const vote = () => {
+    const copy = { ...points }
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  const mostVotedAnecdote = () => {
+    const keys = Object.keys(points);
+    let max = keys[0];
+    keys.forEach((key) => {
+      if (points[key] > points[max]) {
+        max = key;
+      }
+    });
+    return anecdotes[max];
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Ancedote of the day</h1>
+      {anecdotes[selected]}
+      <br></br>
+      <Button handleClick={vote} text='vote' />
+      <Button handleClick={nextAnecdotes} text='next anecdote'/>
+      <h1>Ancedote with the most votes</h1>
+      {mostVotedAnecdote()}
+    </div>
   )
 }
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 
 export default App
--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const handleGoodClick = () => {  setGood(good + 1)}
-const handleNeutralClick = () => {  setNeutral(neutral + 1)}
-const handleBadClick = () => {setBad(bad + 1)}
+  const handleNeutralClick = () => {  setNeutral(neutral + 1)}
+  const handleBadClick = () => {setBad(bad + 1)}
 
 
   return (
@@ -22,28 +22,35 @@ const handleBadClick = () => {setBad(bad + 1)}
   )
 }
 
-const Statistics = (props) => {
-  
+const Statistics = ({good,bad,neutral}) => {
+   const all = good + neutral + bad;
+
+   if(all==0){
+    return(
+      <p>No Feedback Given</p>
+    )
+   }
+
 
   return (
     <>
       <h1>Statistics</h1>
       <table>
       <tbody>
-     
-      <StatisticLine text='good' value={props.good} />
-      <StatisticLine text='neutral' value={props.neutral} />
-      <StatisticLine text='bad' value={props.bad} />
-      <StatisticLine text='all' value={props.good + props.neutral + props.bad} />
-      <StatisticLine text='average' value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-      <StatisticLine text='positive' value={props.good / (props.good + props.neutral + props.bad) * 100 + ' %'} />
+      
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={(good - bad) / (good + neutral + bad)} />
+      <StatisticLine text='positive' value={good / (good + neutral + bad) * 100 + ' %'} />
       </tbody>
 
       </table>
     </>
   )
 }
-const StatisticLine = ({ text, value }) => <tr><td>{text}</td>{value}</tr>
+const StatisticLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
 
